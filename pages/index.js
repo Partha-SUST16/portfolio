11bd2de88586fb8a7ea7093afc430a53d9193194
Partha-SUST16 <ppartha101@gmail.com>
2023-09-06 15:24:14 +0600

    style fix for mobile

--- pages/index.js
+++ pages/index.js
@@ -51,7 +51,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className={`relative ${data.showCursor && "cursor-none"}`}>
+    <div className={`relative ${data.showCursor && "cursor-none"} p-2`}>
       {data.showCursor && <Cursor />}
       <Head>
         <title>{data.name}</title>
@@ -130,7 +130,7 @@ export default function Home() {
         </div>
 
         <div className="mt-10 laptop:mt-30 p-2 laptop:p-0">
-          <h1 className="tablet:m-10 text-2xl text-bold">Achievements.</h1>
+          <h1 className="text-2xl text-bold">Achievements.</h1>
           <div className="mt-5 tablet:m-10 grid grid-cols-1 laptop:grid-cols-2 gap-6">
             {data.services.map((service, index) => (
               <ServiceCard
